69c237ddcd6e9319a450ce0c4974d3f8e1257e33
云却 <479813005@qq.com>
2019-02-18 23:13:30 +0900

    初始化接口库

---
+++ MeguminEngine.Interface/Common1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeguminEngine.Interface
+{
+    public interface IIconID
+    {
+        int IconID { get; set; }
+    }
+}
---
+++ MeguminEngine.Interface/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MeguminEngine.Interface
+{
+    /// <summary>
+    /// 物品主键
+    /// </summary>
+    public interface IItemID
+    {
+        int ItemID { get; set; }
+    }
+
+    public interface IItemInstanceID
+    {
+        long ItemInstanceID { get; set; }
+    }
+
+
+}
