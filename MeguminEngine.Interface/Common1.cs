425bc83e2499c28c09f47cde7aeb9aa3ed7e1d94
云却 <479813005@qq.com>
2020-01-22 00:06:12 +0800

    修正公共接口

--- MeguminEngine.Interface/Common1.cs
+++ MeguminEngine.Interface/Common1.cs
@@ -1,6 +1,5 @@
 ﻿/*
- * 为了减少接口数量，没有将get,set 拆分。
- * 各个实现层注意，如无必要，谨慎使用 set。
+ * 只有Get,接口中无需Set。
  * 
  */
 
@@ -16,52 +15,58 @@ namespace MeguminEngine.Interface
 
     public interface IIconID
     {
-        int IconID { get; set; }
+        int IconID { get; }
     }
 
     public interface ILabel
     {
-        string Label { get; set; }
+        string Label { get; }
     }
 
     public interface IName
     {
-        string Name { get; set; }
+        string Name { get; }
     }
 
     public interface ITags<in K, V>
     {
-        V this[K key] { get; set; }
+        V this[K key] { get; }
     }
 
     public interface ILabels<in K, V>
     {
-        V this[K key] { get; set; }
+        V this[K key] { get; }
     }
 
     public interface INew
     {
-        bool IsNew { get; set; }
+        int IsNew { get; }
     }
 
     public interface IFavorite
     {
-        int? FavoritesTag { get; set; }
+        int? FavoritesTag { get; }
     }
 
+    /// <summary>
+    /// 可选择的
+    /// </summary>
     public interface ISelectable
     {
-        bool IsSelected { get; }
+        int IsSelected { get; }
     }
 
+    /// <summary>
+    /// 可堆叠的
+    /// </summary>
     public interface IStackable
     {
-        int StackCount { get; set; }
+        int StackCount { get; }
     }
 
-    public interface ICount<T> where T : struct
+    public interface ICountable
     {
-        T Count { get; set; }
+        int Count { get; }
     }
 
     /// <summary>
@@ -72,4 +77,91 @@ namespace MeguminEngine.Interface
     {
         (T origibal, T newIntstance) Split(T origibal,int newIntstanceCount);
     }
+
+    /// <summary>
+    /// Area Of Interest
+    /// </summary>
+    public interface IAOIable
+    {
+        /// <summary>
+        /// 是不是感兴趣的 可以监听服务器信息，通常为迷雾外延一格
+        /// </summary>
+        bool Interesting { get; }
+    }
+
+    public interface IConfigID<out T>
+    {
+        /// <summary>
+        /// Gets the config id.
+        /// </summary>
+        T CfgID { get; }
+    }
+
+    public interface IFeatureable
+    {
+        int Feature { get; }
+    }
+
+    /// <summary>
+    /// 可完成的
+    /// </summary>
+    public interface IDoneable
+    {
+        /// <summary>
+        /// 1：成功完成；-1：失败完成；2：取消完成；0： 未完成
+        /// </summary>
+        int IsDone { get; set; }
+    }
+
+    /// <summary>
+    /// 可被写入的/可被修改的
+    /// </summary>
+    public interface IWriteable
+    {
+        /// <summary>
+        /// 0：不可写入； 1： 可以写入
+        /// </summary>
+        int CanWrite { get; }
+    }
+
+    /// <summary>
+    /// 可为起始点的
+    /// </summary>
+    public interface IStartPointable
+    {
+        /// <summary>
+        /// 1: 是起始点
+        /// </summary>
+        int StartPointState { get; }
+    }
+
+    /// <summary>
+    /// 关联的资源
+    /// </summary>
+    public interface IBindAsset
+    {
+        int AssetID { get; }
+    }
+
+    /// <summary>
+    /// 可被加载的
+    /// </summary>
+    public interface ILoadable
+    {
+        /// <summary>
+        /// 0:未开始
+        /// </summary>
+        int LoadState { get; }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ MeguminEngine.Interface/Map.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeguminEngine.Interface
+{
+    /// <summary>
+	/// 可被战争迷雾影响的单位
+	/// </summary>
+	public interface IWarFogable
+    {
+        /// <summary>
+        /// 是否在战争迷雾中
+        /// </summary>
+        int FogState { get; }
+    }
+
+    public interface IPositionable<out T>
+    {
+        /// <summary>
+        /// 对象客户端实际世界坐标
+        /// </summary>
+        T Position { get; }
+    }
+
+    public interface ILocationable<out T>
+    {
+        /// <summary>
+        /// 相对坐标/本地坐标/父空间坐标 
+        /// </summary>
+        T LocalPosition { get; }
+    }
+
+}
